56c3425c22cdae0bdf9f94c29763bfe1aae02b5d
Emanuel Huffman <ehvdb@mst.edu>
2022-06-23 00:44:02 -0400

    testing my branch updates

--- src/components/Hand/Hand.js
+++ src/components/Hand/Hand.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hand = () => {
-  return (
-    <div>Hand</div>
-  )
-}
+  return <div>Hand I am changing this to see the diff in github</div>;
+};
 
-export default Hand
+export default Hand;
